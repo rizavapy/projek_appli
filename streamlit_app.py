77ca371610a71a8b4dbcc27ca527cb1707b59c60
rizavapy <rizava.ankimaka@gmail.com>
2025-07-17 20:02:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+
+background_url = "https://id.images.search.yahoo.com/images/view;_ylt=Awr1Tff883hovx0dCRHNQwx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzRjMmE5MmYwMDA4ZjIyZjkwODRmN2JmYmNkMjVlYmZhBGdwb3MDODMEaXQDYmluZw--?back=https%3A%2F%2Fid.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dbackground%2Bchem%26type%3DE211ID885G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26nost%3D1%26tab%3Dorganic%26ri%3D83&w=1280&h=720&imgurl=i.ytimg.com%2Fvi%2FYDcGlzUgDBI%2Fmaxresdefault.jpg&rurl=https%3A%2F%2Far.inspiredpencil.com%2Fpictures-2023%2Fchemistry-background&size=95KB&p=background+chem&oid=4c2a92f0008f22f9084f7bfbcd25ebfa&fr2=piv-web&fr=mcafee&tt=Chemistry+Background&b=61&ni=21&no=83&ts=&tab=organic&sigr=ZVFQS4ZLhYlG&sigb=vM3FnucgGh8s&sigi=Ot9wi9QaiUV8&sigt=6UiRB1F.smSx&.crumb=BIFLTqJlF5S&fr=mcafee&fr2=piv-web&type=E211ID885G0"
+
 st.markdown(f"""
     <style>
     .stApp {{
@@ -14,6 +18,8 @@ st.markdown(f"""
     </style>
 """, unsafe_allow_html=True)
 
+st.title("🎉 Selamat Datang di UncertaintyCalc")
+st.write("Ini adalah tampilan dengan background gambar.")
 # === Konten Aplikasi ===
 st.title("🔬 Selamat Datang di UncertaintyCalc")
 st.write("Ini adalah demo tampilan dengan gambar background dan efek transparansi.")
