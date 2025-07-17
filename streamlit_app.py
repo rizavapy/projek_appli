656d669a7a3d5086aa90b336825bf9033a47ca85
rizavapy <rizava.ankimaka@gmail.com>
2025-07-17 20:10:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.markdown("""
     <style>
     .stApp {
-        background-image: url("https://tse3.mm.bing.net/th/id/OIP.I3r43qXhEZO2e5JDGnaCBQHaE8?pid=Api&P=0&h=180");
+        background-image: url("https://images.unsplash.com/photo-1603791452906-3b3db9a7f58a");
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
